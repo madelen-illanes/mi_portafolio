7ac2313a75a7d746978a5c2dd3be7d9d7a15853f
madelen-illanes <madelen2811@gmail.com>
2023-02-24 12:35:53 -0500

    estuctura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
